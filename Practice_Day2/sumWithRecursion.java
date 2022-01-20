7fc8a44b8ae2ac1ed19ab71d25de57d9a9a30d58
Anubhav Joshi <63429475+Anubhavjoshi040@users.noreply.github.com>
2022-01-21 01:02:27 +0530

    Practice_Day2

---
+++ Practice_Day2/sumWithRecursion.java
@@ -0,0 +1,21 @@
+// Anubhav Joshi
+// Sum of n natural numbers using recursion
+package com.anubhav.pep.day2;
+import java.util.Scanner;
+public class sumWithRecursion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        sum(num);
+        System.out.println(result);
+    }
+    static int result = 0;
+    static void sum(int n){
+        if(n == 0){
+            return;
+        }
+        result += n;
+        n--;
+        sum(n);
+    }
+}
