d0d3b217da9e382b1d1af76b154feb8598d15873
Anubhav Joshi <63429475+Anubhavjoshi040@users.noreply.github.com>
2022-01-24 01:25:04 +0530

    Practice_Day3

---
+++ Practice_Day3/MatrixSum.java
@@ -0,0 +1,45 @@
+// Anubhav Joshi
+package com.anubhav.pep.day3;
+import java.util.Scanner;
+public class MatrixSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the 1st size of 1st 2D array: ");
+        int size1 = input.nextInt();
+        int[][] arr1 = new int[size1][size1];
+        System.out.println("Enter the size of 2nd 2D array: ");
+        int size2 = input.nextInt();
+        int[][] arr2 = new int[size2][size2];
+        System.out.println("Enter the values elements of 1st Array: ");
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size1; j++) {
+                arr1[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Enter the values elements of 2nd Array: ");
+        for (int i = 0; i < size2; i++) {
+            for (int j = 0; j < size2; j++) {
+                arr2[i][j] = input.nextInt();
+            }
+        }
+        int[][] arrSum = sum(arr1, arr2, size1);
+        System.out.println("Result: ");
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                System.out.print(arrSum[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+    static int[][] sum(int[][] a1, int[][] a2, int s){
+        int[][] arrSum  = new int[s][s];
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < s; j++) {
+                arrSum[i][j] = a1[i][j] + a2[i][j];
+            }
+        }
+
+        return arrSum;
+    }
+}
